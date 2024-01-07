adc7fa27d179caf2a56f47795d6e07144118254d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-01-07 22:54:42 +0100

    fixed merge

--- web_server/order/share.js
+++ web_server/order/share.js
@@ -1,5 +1,4 @@
 import * as server from "../server.js"
-export let price_array = [];
 export function set_cart_items(cart_items) {
 
     localStorage.setItem('cartitems', JSON.stringify(cart_items));
@@ -18,6 +17,9 @@ export function reset_cart_total() {
 }
 
 export const color_array = ["red", "green", "blue", "yellow"];
+
+export let price_array = [];
+
 
 export async function update_price_array() {
     let data = await server.get_product_pages();
